9210fbdba9214fd4e046d7f68662b5973a90d7d5
root <cgc940405@gmail.com>
2022-03-20 00:28:56 +0900

    create .env & involved at gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAhfo0cQE4pUmNnHiw1MH-l70NT0Im_UJo',
-    authDomain: 'nwitter-18eca.firebaseapp.com',
-    projectId: 'nwitter-18eca',
-    storageBucket: 'nwitter-18eca.appspot.com',
-    messagingSenderId: '408183737995',
-    appId: '1:408183737995:web:19dad617f14f97718a0e8e',
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
