6ec81d28d43d3b08d1da5997be17b457a8a50040
root <cgc940405@gmail.com>
2022-03-21 06:14:36 +0900

    build Router Structure

---
+++ src/components/Router.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <HashRouter>
+            <Routes>
+                {isLoggedIn ? (
+                    <>
+                        <Route exact path="/" element={<Home />}></Route>
+                    </>
+                ) : (
+                    <Route exact path="/" element={<Auth />}></Route>
+                )}
+            </Routes>
+        </HashRouter>
+    );
+};
+
+export default AppRouter;
